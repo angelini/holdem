65c4540ab3d2a16586cfd58e04a1960337f6e249
Alex Angelini <alex.louis.angelini@gmail.com>
2018-12-01 18:00:55 -0500

    Add shared gce utils to scripts

--- scripts/ssh_repl.sh
+++ scripts/ssh_repl.sh
@@ -1,4 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-gcloud compute ssh --zone=us-east1-b web-1 -- -N -L 7001:127.0.0.1:7001 &
+set -eu
+
+SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd)"
+
+# shellcheck source=./config.sh
+source "${SCRIPTS_DIR}/config.sh"
+
+g_compute ssh "${SSH_NODE}" -- -N -L 7001:127.0.0.1:7001 &
+
 lein repl :connect 7001
+kill 0
